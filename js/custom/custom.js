8e9c3e558063776ecb98adb76dd9a3ca248e2404
雨城 <zg20020713zy@163.com>
2022-05-13 21:52:39 +0800

    Site updated: 2022-05-13 21:52:37

---
+++ js/custom/custom.js
@@ -0,0 +1,5 @@
+ var btn = document.querySelector('.fa-search');
+
+ window.onload = function(){
+    btn.className = 'fa-fw iconfont icon-sousuo'
+};
--- js/randombg.js
+++ js/randombg.js
@@ -1,12 +1,16 @@
-
-
-
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
   "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/1efe8fad-cd80-4a89-bd1d-5bc03c4b517d.jpg",
   "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/1600590648514.jpeg",
   "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/1600590688678.jpeg",
-  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/1600590716552.jpeg"
+  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/1600590716552.jpeg",
+  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/1600590632348%281%29.jpeg",
+  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/000000058.webp",
+  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/000000067.webp",
+  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/000000093.webp",
+  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/78273944_p0_master1200%281%29.jpg",
+  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/79169571_p0_master1200%281%29.jpg",
+  "https://yuchenimage.oss-cn-shanghai.aliyuncs.com/background/80833168_p0_master1200.jpg"
 ];
 
 //重设背景图片
